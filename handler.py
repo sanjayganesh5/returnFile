4c3dabb43dddadd27f07fe59d09ffca193d84b78
sanjayganesh5 <106419569+sanjayganesh5@users.noreply.github.com>
2023-07-18 00:53:29 +0530

    Update handler.py, with buffer

--- handler.py
+++ handler.py
@@ -1,5 +1,6 @@
 from openpyxl import load_workbook
 from flask import send_file
+from io import BytesIO
 
 
 def lambda_handler(event, context):
@@ -7,5 +8,7 @@ def lambda_handler(event, context):
     sheet = wb['Sheet1']
     # Save the manipulated Excel data to a buffer
     sheet['A2'] = 'SanjayGanesh'
-    wb.save('output.xlsx')
-    return send_file('output.xlsx', as_attachment=True, download_name='output.xlsx')
+    buffer = BytesIO
+    wb.save(buffer)
+    buffer.seek(0)
+    return send_file(buffer, as_attachment=True, download_name='output.xlsx')
