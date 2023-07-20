0e78ab5b74ffe7c63f3c379cf00d722654ec77d5
sanjayganesh5 <106419569+sanjayganesh5@users.noreply.github.com>
2023-07-20 16:11:49 +0530

    Update handler.py

--- handler.py
+++ handler.py
@@ -6,13 +6,13 @@ import base64
 def lambda_handler(event, context):
     wb = load_workbook(filename='template.xlsx',
                        read_only=False)
-    sheet = wb['Sheet1']
+    sheet = wb.active
     # Save the manipulated Excel data to a buffer
-    sheet['A2'] = 'SanjayGanesh'
+    sheet.cell(row=2, column=1, value='SanjayGanesh')
     buffer = BytesIO
     wb.save(buffer)
     buffer.seek(0)
-    updated_excel = buffer.getvalue()
+    updated_excel = buffer.read()
     encoded_excel = base64.b64encode(updated_excel).decode('utf-8')
     return {
         'statusCode': 200,
